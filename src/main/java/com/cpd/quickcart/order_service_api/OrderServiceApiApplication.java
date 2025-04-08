8e485fca6b12526566bce4904fb25ab9131d4956
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-04-08 20:34:03 +0530

    customerOrder entity done

---
+++ src/main/java/com/cpd/quickcart/order_service_api/OrderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.quickcart.order_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApiApplication.class, args);
+	}
+
+}
