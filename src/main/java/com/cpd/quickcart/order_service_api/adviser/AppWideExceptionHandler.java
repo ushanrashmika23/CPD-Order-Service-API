02f5a98bbd8bf0d562eb1fdc53cfdc5b6300e215
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-04-11 17:23:22 +0530

    CustomerOrder EntryNotFoundException done

--- src/main/java/com/cpd/quickcart/order_service_api/adviser/AppWideExceptionHandler.java
+++ src/main/java/com/cpd/quickcart/order_service_api/adviser/AppWideExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.cpd.quickcart.order_service_api.adviser;
 
-import com.cpd.quickcart.order_service_api.exception.EntryNotFoundExeption;
+import com.cpd.quickcart.order_service_api.exception.EntryNotFoundException;
 import com.cpd.quickcart.order_service_api.util.StanderdResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AppWideExceptionHandler {
-    public ResponseEntity<StanderdResponseDto> handleEntryNotFoundException(EntryNotFoundExeption e){
+    public ResponseEntity<StanderdResponseDto> handleEntryNotFoundException(EntryNotFoundException e){
         return new ResponseEntity<>(
                new  StanderdResponseDto(
                         200,
